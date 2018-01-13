1f8a2f693517a576f999d33b02c859eab2444aa0
Robert WC Seifert <rseifert.phone@gmail.com>
2018-01-13 17:21:09 -0500

    Sync

--- src/main/scala/com/builtbroken/mc/prefab/gui/slot/SlotEnergyItem.java
+++ src/main/scala/com/builtbroken/mc/prefab/gui/slot/SlotEnergyItem.java
@@ -1,19 +1,19 @@
 package com.builtbroken.mc.prefab.gui.slot;
 
-import com.builtbroken.mc.client.SharedAssets;
-import com.builtbroken.mc.framework.energy.UniversalEnergySystem;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.Slot;
-import net.minecraft.item.ItemStack;
+        import com.builtbroken.mc.client.SharedAssets;
+        import com.builtbroken.mc.framework.energy.UniversalEnergySystem;
+        import net.minecraft.client.Minecraft;
+        import net.minecraft.client.gui.Gui;
+        import net.minecraft.inventory.IInventory;
+        import net.minecraft.inventory.Slot;
+        import net.minecraft.item.ItemStack;
 
 public class SlotEnergyItem extends Slot implements ISlotRender
 {
-	public SlotEnergyItem(IInventory inv, int par3, int par4, int par5)
-	{
-		super(inv, par3, par4, par5);
-	}
+    public SlotEnergyItem(IInventory inv, int par3, int par4, int par5)
+    {
+        super(inv, par3, par4, par5);
+    }
 
     @Override
     public boolean isItemValid(ItemStack compareStack)
