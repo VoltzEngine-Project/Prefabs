fcae561d1ea61674eea7d31e51a1d9c7b70bcd19
Robert WC Seifert <rseifert.phone@gmail.com>
2018-02-20 04:38:27 -0500

    Fixed inf loop for RestorePrevGui

--- src/main/scala/com/builtbroken/mc/prefab/gui/event/RestorePrevGui.java
+++ src/main/scala/com/builtbroken/mc/prefab/gui/event/RestorePrevGui.java
@@ -8,6 +8,9 @@ import java.util.function.Consumer;
 
 /**
  * Simple consume function for use with restoring GUIs
+ * <p>
+ * Do not run this in {@link GuiScreen#onGuiClosed()} as it can easily
+ * inf loop if not setup correctly. Instead use a dedicated action or button.
  *
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 2/20/2018.
@@ -22,12 +25,21 @@ public class RestorePrevGui implements Consumer<GuiScreen>
     }
 
     @Override
-    public void accept(GuiScreen guiScreen)
+    public void accept(GuiScreen currentGUI)
     {
         if (prev_gui != null && !(prev_gui instanceof GuiContainer))
         {
-            //TODO build a list of GUIs that fail or shouldn't be opened
-            Minecraft.getMinecraft().displayGuiScreen(prev_gui);
+            if (Minecraft.getMinecraft().currentScreen != prev_gui)
+            {
+                //Trigger cleanup on previous GUI
+                if (currentGUI != null)
+                {
+                    currentGUI.onGuiClosed();
+                }
+
+                //TODO build a list of GUIs that fail or shouldn't be opened
+                Minecraft.getMinecraft().displayGuiScreen(prev_gui);
+            }
         }
     }
 }
