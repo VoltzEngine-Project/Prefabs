7163d12d35a58f2ae3b97171b5558c5c5ee2e81a
Robert WC Seifert <rseifert.phone@gmail.com>
2018-03-03 11:19:50 -0500

    Rewrote GuiLabel

--- src/main/scala/com/builtbroken/mc/prefab/gui/components/GuiLabel.java
+++ src/main/scala/com/builtbroken/mc/prefab/gui/components/GuiLabel.java
@@ -1,6 +1,7 @@
 package com.builtbroken.mc.prefab.gui.components;
 
 import net.minecraft.client.Minecraft;
+import org.lwjgl.opengl.GL11;
 
 /**
  * Version of {@link GuiField} that can not be edited
@@ -8,26 +9,75 @@ import net.minecraft.client.Minecraft;
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 4/30/2017.
  */
-public class GuiLabel extends GuiField
+public class GuiLabel extends GuiComponent<GuiLabel>
 {
-    //TODO rewrite to not extend GuiField
+    /** Has the current text being edited on the textbox. */
+    private String text = "";
+
     public GuiLabel(int x, int y, String label)
     {
-        super(x, y);
+        super(-1, x, y);
         setText(label);
-        setEnableBackgroundDrawing(false);
-        setEnabled(false);
     }
 
-    @Override
-    public boolean mousePressed(Minecraft mc, int mouseX, int mouseY)
+    /**
+     * Sets the text of the textbox
+     */
+    public void setText(String text)
     {
-        return false;
+        if (text != null)
+        {
+            this.text = text;
+        }
+        else
+        {
+            setText("");
+        }
+    }
+
+    /**
+     * Returns the contents of the textbox
+     */
+    public String getText()
+    {
+        return this.text;
     }
 
     @Override
+    protected void preRender(Minecraft mc, int mouseX, int mouseY)
+    {
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glDisable(GL11.GL_BLEND);
+    }
+
+    @Override
+    protected void postRender(Minecraft mc, int mouseX, int mouseY)
+    {
+        GL11.glEnable(GL11.GL_LIGHTING);
+        GL11.glEnable(GL11.GL_BLEND);
+    }
+
+    @Override
+    protected void drawBackground(Minecraft mc, int mouseX, int mouseY)
+    {
+
+    }
+
+    @Override
+    protected void doRender(Minecraft mc, int mouseX, int mouseY)
+    {
+        super.doRender(mc, mouseX, mouseY);
+        final int textRenderColor = getTextColor();
+        String displayString = Minecraft.getMinecraft().fontRenderer.trimStringToWidth(this.text, this.getWidth());
+        Minecraft.getMinecraft().fontRenderer.drawStringWithShadow(displayString, x(), y(), textRenderColor);
+    }
+
     protected int getTextColor()
     {
+        if (!isEnabled())
+        {
+            return 7368816;
+        }
         return 14737632;
     }
 }
